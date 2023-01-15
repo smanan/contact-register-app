2b9273a3e668795871e599486ea082b8c62b9459
Manan <smanan097@gmail.com>
2023-01-15 19:49:58 +0530

    update

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
-const ContactForm = () => {
+const ContactForm = (props) => {
+  const initialValues = { full_name: "", phone_number: "" };
+  const [form, setForm] = React.useState(initialValues);
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: [e.target.value] });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (form.full_name === "" || form.phone_number === "") {
+      alert("Please Enter The Data");
+    }
+  };
   return (
-    <div>ContactForm</div>
-  )
-}
+    <form>
+      <div>
+        <input type="text" name="full_name" placeholder="Full name" />
+      </div>
 
-export default ContactForm
+      <div>
+        <input
+          type="number"
+          name="phone_number"
+          placeholder="Phone Number"
+          maxLength={10}
+          minLength={10}
+        />
+      </div>
+
+      <div id="button">
+        <button>Add</button>
+      </div>
+    </form>
+  );
+};
+
+export default ContactForm;
